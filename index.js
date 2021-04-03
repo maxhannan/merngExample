31755a4a4d5954228019fb8916af17fc746b24ec
maxhannan <maxhannan@substanceseven.com>
2021-04-03 14:52:02 -0500

    inital commit and server code

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphQL/typeDefs')
+const resolvers = require('./graphQL/resolvers')
+const { MONGODB } = require('./config');
+
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=> {
+    console.log('MongoDB Connected')
+    return server.listen({ port: 5000})
+  })
+  .then(res => {
+    console.log(`Server running at ${res.url}`)
+  })
